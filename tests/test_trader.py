709f12434d32eb75c491503b01a44a4edc560d3a
chesswiz16 <3wood.tiger@gmail.com>
2018-01-07 22:23:59 -0500

    Product lookup on startup

--- tests/test_trader.py
+++ tests/test_trader.py
@@ -3,6 +3,7 @@ import uuid
 
 from trader.base_trader import Trader
 from trader.base_trader import OrderPlacementFailure
+from trader.base_trader import ProductDefinitionFailure
 
 
 class AuthenticatedClientMock(object):
@@ -21,6 +22,30 @@ class AuthenticatedClientMock(object):
                 'side': side,
             }
 
+    # noinspection PyMethodMayBeStatic
+    def get_products(self):
+        return [
+            {
+                'status': 'online',
+                'id': 'BTC-USD',
+                'quote_increment': 0.5,
+            },
+            {
+                'status': 'online',
+                'id': 'ETH-USD',
+                'quote_increment': 0.01,
+            },
+            {
+                'status': 'offline',
+                'id': 'ETH-EUR',
+                'quote_increment': 0.1,
+            }
+        ]
+
+    # noinspection PyMethodMayBeStatic
+    def get_orders(self):
+        return [[]]
+
     def buy(self, **kwargs):
         return self.mock_trade('buy', kwargs['price'], kwargs['size'])
 
@@ -28,40 +53,55 @@ class AuthenticatedClientMock(object):
         return self.mock_trade('sell', kwargs['price'], kwargs['size'])
 
 
-class TestTrader(unittest.TestCase):
-    def setUp(self):
-        self.trader = Trader(AuthenticatedClientMock(), 'BTC-USD', 0.5)
+class AuthenticatedClientMockWithOrders(AuthenticatedClientMock):
+    def get_orders(self):
+        return [[
+            {
+                'id': 'test_id',
+                'side': 'buy',
+                'size': '100',
+                'price': '100',
+            }
+        ]]
 
+
+class TestTrader(unittest.TestCase):
     def test_increment(self):
-        self.trader.buy_limit_ptc(50, 100.3)
+        btc_trader = Trader(AuthenticatedClientMock(), 'BTC-USD')
+        btc_trader.buy_limit_ptc(50, 100.3)
         expected = {
             'side': 'buy',
             'size': '50',
             'price': '100.5',
         }
         self.assertIn(expected, [{'side': x['side'], 'size': x['size'], 'price': x['price']} for x in
-                                 self.trader.orders.values()])
-        self.trader.quote_increment = 0.01
-        self.trader.buy_limit_ptc(50, 100.03)
+                                 btc_trader.orders.values()])
+        eth_trader = Trader(AuthenticatedClientMock(), 'ETH-USD')
+        eth_trader.buy_limit_ptc(50, 100.03)
         expected = {
             'side': 'buy',
             'size': '50',
             'price': '100.03',
         }
         self.assertIn(expected, [{'side': x['side'], 'size': x['size'], 'price': x['price']} for x in
-                                 self.trader.orders.values()])
+                                 eth_trader.orders.values()])
+
+    def test_bad_product(self):
+        self.assertRaises(ProductDefinitionFailure, Trader, AuthenticatedClientMock(), 'ETH-EUR')
 
     def test_good_order(self):
-        self.trader.buy_limit_ptc(10, 100)
-        self.assertTrue(len(self.trader.orders) == 1)
-        self.trader.buy_limit_ptc(10, 200)
-        self.assertTrue(len(self.trader.orders) == 2)
-        self.trader.sell_limit_ptc(10, 200)
-        self.assertTrue(len(self.trader.orders) == 3)
+        trader = Trader(AuthenticatedClientMockWithOrders(), 'BTC-USD')
+        trader.buy_limit_ptc(10, 100)
+        self.assertTrue(len(trader.orders) == 2)
+        trader.buy_limit_ptc(10, 200)
+        self.assertTrue(len(trader.orders) == 3)
+        trader.sell_limit_ptc(10, 200)
+        self.assertTrue(len(trader.orders) == 4)
 
     def test_bad_order(self):
-        self.assertRaises(OrderPlacementFailure, self.trader.buy_limit_ptc, 200, 10)
-        self.assertRaises(OrderPlacementFailure, self.trader.sell_limit_ptc, 200, 10)
+        trader = Trader(AuthenticatedClientMock(), 'BTC-USD')
+        self.assertRaises(OrderPlacementFailure, trader.buy_limit_ptc, 200, 10)
+        self.assertRaises(OrderPlacementFailure, trader.sell_limit_ptc, 200, 10)
 
 
 if __name__ == '__main__':
--- trader/base_trader.py
+++ trader/base_trader.py
@@ -1,9 +1,16 @@
 class Trader(object):
-    def __init__(self, client, product_id, quote_increment):
+    def __init__(self, client, product_id):
         self.client = client
         self.product_id = product_id
-        self.quote_increment = quote_increment
-        self.orders = dict()
+        # Query for product and find status/increment
+        products = self.client.get_products()
+        product = [x for x in products if x['status'] == 'online' and x['id'] == product_id]
+        if len(product) != 1:
+            raise ProductDefinitionFailure(product_id + ' not active for trading')
+        self.quote_increment = product[0]['quote_increment']
+        # Query for current open orders
+        orders = self.client.get_orders()[0]
+        self.orders = dict(zip([x['id'] for x in orders], orders))
 
     def buy_limit_ptc(self, size, price):
         """Post only limit buy"""
@@ -43,6 +50,14 @@ class OrderPlacementFailure(Exception):
         return repr(self.parameter)
 
 
+class ProductDefinitionFailure(Exception):
+    def __init__(self, value):
+        self.parameter = value
+
+    def __str__(self):
+        return repr(self.parameter)
+
+
 if __name__ == '__main__':
     import json
     from gdax.authenticated_client import AuthenticatedClient
@@ -50,10 +65,13 @@ if __name__ == '__main__':
     with open('../config/sandbox.json') as config:
         data = json.load(config)
 
-    client = AuthenticatedClient(data["auth"]["key"],
-                                 data["auth"]["secret"],
-                                 data["auth"]["phrase"],
-                                 api_url=data["endpoints"]["rest"])
-
-    res = client.get_accounts()
+    auth_client = AuthenticatedClient(data["auth"]["key"],
+                                      data["auth"]["secret"],
+                                      data["auth"]["phrase"],
+                                      api_url=data["endpoints"]["rest"])
+    t = Trader(auth_client, 'BTC-USD')
+    # t.buy_limit_ptc(1, 100.001)
+    res = auth_client.get_products()
+    print(json.dumps(res, indent=4, sort_keys=True))
+    res = auth_client.get_orders()
     print(json.dumps(res, indent=4, sort_keys=True))
