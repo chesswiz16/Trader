6b4df34259b61c1b09ce8fcffdc8c087ed606fb8
chesswiz16 <3wood.tiger@gmail.com>
2018-01-08 23:35:55 -0500

    Seed wallet

--- tests/test_trader.py
+++ tests/test_trader.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 class AuthenticatedClientMock(object):
     # noinspection PyMethodMayBeStatic
-    def mock_trade(self, side, price, size):
+    def mock_trade(self, side, price, size, order_type, post_only):
         """Mock failures if quantity > 100"""
         if size > 100:
             return {
@@ -20,6 +20,8 @@ class AuthenticatedClientMock(object):
                 'price': str(price),
                 'size': str(size),
                 'side': side,
+                'type': order_type,
+                'post_only': post_only,
             }
 
     # noinspection PyMethodMayBeStatic
@@ -28,23 +30,29 @@ class AuthenticatedClientMock(object):
             {
                 'status': 'online',
                 'id': 'BTC-USD',
-                'quote_increment': 0.5,
+                'quote_increment': '0.5',
                 'base_currency': 'BTC',
                 'quote_currency': 'USD',
+                'base_min_size': '0.5',
+                'base_max_size': '10000',
             },
             {
                 'status': 'online',
                 'id': 'ETH-USD',
-                'quote_increment': 0.01,
+                'quote_increment': '0.01',
                 'base_currency': 'ETC',
                 'quote_currency': 'USD',
+                'base_min_size': '0.01',
+                'base_max_size': '100000',
             },
             {
                 'status': 'offline',
                 'id': 'ETH-EUR',
-                'quote_increment': 0.1,
+                'quote_increment': '0.1',
                 'base_currency': 'ETC',
                 'quote_currency': 'EUR',
+                'base_min_size': '0.01',
+                'base_max_size': '100000',
             }
         ]
 
@@ -53,10 +61,10 @@ class AuthenticatedClientMock(object):
         return [[]]
 
     def buy(self, **kwargs):
-        return self.mock_trade('buy', kwargs['price'], kwargs['size'])
+        return self.mock_trade('buy', kwargs['price'], kwargs['size'], kwargs['type'], kwargs.get('post_only', False))
 
     def sell(self, **kwargs):
-        return self.mock_trade('sell', kwargs['price'], kwargs['size'])
+        return self.mock_trade('sell', kwargs['price'], kwargs['size'], kwargs['type'], kwargs.get('post_only', False))
 
     # noinspection PyMethodMayBeStatic
     def get_accounts(self):
@@ -70,6 +78,12 @@ class AuthenticatedClientMock(object):
                 'available': "100000.001",
             }
         ]
+
+    # noinspection PyUnusedLocal,PyMethodMayBeStatic
+    def get_product_ticker(self, product_id):
+        return {
+            'price': '100'
+        }
 
 
 class AuthenticatedClientMockWithOrders(AuthenticatedClientMock):
@@ -103,18 +117,20 @@ class TestTrader(unittest.TestCase):
             'side': 'buy',
             'size': '50',
             'price': '100.5',
+            'type': 'limit',
+            'post_only': True,
         }
-        self.assertIn(expected, [{'side': x['side'], 'size': x['size'], 'price': x['price']} for x in
-                                 btc_trader.orders.values()])
+        self.assertIn(expected, [{k: x[k] for k in expected.keys()} for x in btc_trader.orders.values()])
         eth_trader = Trader(AuthenticatedClientMock(), 'ETH-USD')
         eth_trader.buy_limit_ptc(50, 100.03)
         expected = {
             'side': 'buy',
             'size': '50',
             'price': '100.03',
+            'type': 'limit',
+            'post_only': True,
         }
-        self.assertIn(expected, [{'side': x['side'], 'size': x['size'], 'price': x['price']} for x in
-                                 eth_trader.orders.values()])
+        self.assertIn(expected, [{k: x[k] for k in expected.keys()} for x in eth_trader.orders.values()])
 
     def test_bad_product(self):
         self.assertRaises(ProductDefinitionFailure, Trader, AuthenticatedClientMock(), 'ETH-EUR')
@@ -127,16 +143,42 @@ class TestTrader(unittest.TestCase):
         self.assertTrue(len(trader.orders) == 3)
         trader.sell_limit_ptc(10, 200)
         self.assertTrue(len(trader.orders) == 4)
+        trader.buy_stop(10, 100)
+        self.assertTrue(len(trader.orders) == 5)
 
     def test_bad_order(self):
         trader = Trader(AuthenticatedClientMock(), 'BTC-USD')
         self.assertRaises(OrderPlacementFailure, trader.buy_limit_ptc, 200, 10)
         self.assertRaises(OrderPlacementFailure, trader.sell_limit_ptc, 200, 10)
+        self.assertRaises(OrderPlacementFailure, trader.sell_limit_ptc, 20000, 10)
+        self.assertRaises(OrderPlacementFailure, trader.sell_limit_ptc, 0.001, 10)
 
     def test_insufficient_funds(self):
         trader = Trader(AuthenticatedClientMockWithNoBalance(), 'BTC-USD')
         self.assertRaises(AccountBalanceFailure, trader.buy_limit_ptc, 50, 10)
         self.assertRaises(AccountBalanceFailure, trader.sell_limit_ptc, 50, 10)
+
+    def test_seed_wallet(self):
+        trader = Trader(AuthenticatedClientMock(), 'BTC-USD')
+        trader.seed_wallet(20)
+        expected = [
+            {
+                'side': 'buy',
+                'size': '20',
+                'price': '101.0',
+                'type': 'stop',
+                'post_only': False,
+            },
+            {
+                'side': 'buy',
+                'size': '20',
+                'price': '99.0',
+                'type': 'limit',
+                'post_only': True,
+            },
+        ]
+        for order in expected:
+            self.assertIn(order, [{k: x[k] for k in order.keys()} for x in trader.orders.values()])
 
 
 if __name__ == '__main__':
--- trader/base_trader.py
+++ trader/base_trader.py
@@ -13,65 +13,110 @@ class Trader(object):
         products = self.client.get_products()
         product = [x for x in products if x['status'] == 'online' and x['id'] == product_id]
         if len(product) != 1:
-            module_logger.error('Product {} invalid from products {}'.format(product_id, json.dumps(products, indent=4,
-                                                                                                    sort_keys=True)))
+            module_logger.error('Product {} invalid from products {}'.format(
+                product_id, json.dumps(products, indent=4, sort_keys=True)))
             raise ProductDefinitionFailure(product_id + ' not active for trading')
-        self.quote_increment = product[0]['quote_increment']
         self.base_currency = product[0]['base_currency']
         self.quote_currency = product[0]['quote_currency']
+        self.quote_increment = float(product[0]['quote_increment'])
+        self.base_min_size = float(product[0]['base_min_size'])
+        self.base_max_size = float(product[0]['base_max_size'])
 
         # Query for current open orders
         orders = self.client.get_orders()[0]
         self.orders = dict(zip([x['id'] for x in orders], orders))
         module_logger.info(
-            'Started trader on {} with increment {} and {} open orders'.format(product_id, self.quote_increment,
-                                                                               len(self.orders)))
+            'Started trader on {} with increment {} and {} open orders'.format(
+                product_id, self.quote_increment, len(self.orders)))
 
-    # on_start
+    def seed_wallet(self, size):
+        """At the start of the day or when the wallet is empty, need something to trade
+        Place a stop buy at 1% above current market and a limit post buy at 1% below to minimize fees
+        """
+        ticker = self.client.get_product_ticker(self.product_id)
+        if 'price' not in ticker:
+            module_logger.exception('Unable to get current market quote')
+            raise OrderPlacementFailure(
+                'Unable to get market quote, api message: {}'.format(ticker.get('message', 'unknown error')))
+        current_price = float(ticker['price'])
+        delta = current_price * 0.01
+        module_logger.info(
+            'Seeding wallet: {} {} @ {}/{}'.format(size, self.product_id, current_price + delta, current_price - delta))
+        self.buy_stop(size, current_price + delta)
+        self.buy_limit_ptc(size, current_price - delta)
+
     # on_fill
 
-    def buy_limit_ptc(self, size, price):
-        """Post only limit buy"""
+    def buy_stop(self, size, price):
         balance = self.get_balance(self.quote_currency)
         if balance < size * price:
             module_logger.exception('Insufficient funds for buy of {}, current balance {}'.format(size, balance))
             raise AccountBalanceFailure('Needed {} have {}'.format(size * price, balance))
-        result = self.client.buy(type='limit',
-                                 product_id=self.product_id,
-                                 price=self.to_increment(price),
-                                 size=size,
-                                 post_only=True)
+        if size < self.base_min_size or size > self.base_max_size:
+            raise OrderPlacementFailure('Size of {} outside of exchange limits'.format(size))
+        result = self.client.buy(
+            type='stop',
+            product_id=self.product_id,
+            price=self.to_increment(price),
+            size=size,
+        )
         if 'message' in result:
             module_logger.exception('Error placing buy order, message from api: {}'.format(result['message']))
             raise OrderPlacementFailure(result['message'])
         else:
             self.orders[result['id']] = result
-        module_logger.info('Placed buy order for {} {} @ {}'.format(size, self.product_id, price))
+        module_logger.info('Placed buy stop order for {} {} @ {}'.format(size, self.product_id, price))
+
+    def buy_limit_ptc(self, size, price):
+        """Post only limit buy with validations"""
+        balance = self.get_balance(self.quote_currency)
+        if balance < size * price:
+            module_logger.exception('Insufficient funds for buy of {}, current balance {}'.format(size, balance))
+            raise AccountBalanceFailure('Needed {} have {}'.format(size * price, balance))
+        if size < self.base_min_size or size > self.base_max_size:
+            raise OrderPlacementFailure('Size of {} outside of exchange limits'.format(size))
+        result = self.client.buy(
+            type='limit',
+            product_id=self.product_id,
+            price=self.to_increment(price),
+            size=size,
+            post_only=True,
+        )
+        if 'message' in result:
+            module_logger.exception('Error placing buy order, message from api: {}'.format(result['message']))
+            raise OrderPlacementFailure(result['message'])
+        else:
+            self.orders[result['id']] = result
+        module_logger.info('Placed buy limit order for {} {} @ {}'.format(size, self.product_id, price))
 
     def sell_limit_ptc(self, size, price):
-        """Post only limit sell"""
+        """Post only limit sell with validations"""
         balance = self.get_balance(self.base_currency)
         if balance < size:
             module_logger.exception('Insufficient funds for sell of {}, current balance {}'.format(size, balance))
             raise AccountBalanceFailure('Needed {} have {}'.format(size, balance))
-        result = self.client.sell(type='limit',
-                                  product_id=self.product_id,
-                                  price=self.to_increment(price),
-                                  size=size,
-                                  post_only=True)
+        if size < self.base_min_size or size > self.base_max_size:
+            raise OrderPlacementFailure('Size of {} outside of exchange limits'.format(size))
+        result = self.client.sell(
+            type='limit',
+            product_id=self.product_id,
+            price=self.to_increment(price),
+            size=size,
+            post_only=True,
+        )
         if 'message' in result:
             module_logger.exception('Error placing sell order, message from api: {}'.format(result['message']))
             raise OrderPlacementFailure(result['message'])
         else:
             self.orders[result['id']] = result
-        module_logger.info('Placed sell order for {} {} @ {}'.format(size, self.product_id, price))
+        module_logger.info('Placed sell limit order for {} {} @ {}'.format(size, self.product_id, price))
 
     def get_balance(self, currency):
         accounts = self.client.get_accounts()
         account = [x for x in accounts if x['currency'] == currency]
         if len(account) != 1 or 'available' not in account[0]:
-            module_logger.error('Account lookup failure for {} from {}'.format(currency, json.dumps(accounts, indent=4,
-                                                                                                    sort_keys=True)))
+            module_logger.error('Account lookup failure for {} from {}'.format(
+                currency, json.dumps(accounts, indent=4, sort_keys=True)))
             raise AccountBalanceFailure(currency + ' not found in active accounts')
         return float(account[0]['available'])
 
@@ -108,16 +153,20 @@ class ProductDefinitionFailure(Exception):
 if __name__ == '__main__':
     from gdax.authenticated_client import AuthenticatedClient
 
-    with open('../config/sandbox.json') as config:
+    with open('../config/prod.json') as config:
         data = json.load(config)
 
-    auth_client = AuthenticatedClient(data["auth"]["key"],
-                                      data["auth"]["secret"],
-                                      data["auth"]["phrase"],
-                                      api_url=data["endpoints"]["rest"])
-    t = Trader(auth_client, 'BTC-USD')
-    # t.buy_limit_ptc(1, 100.001)
+    auth_client = AuthenticatedClient(
+        data["auth"]["key"],
+        data["auth"]["secret"],
+        data["auth"]["phrase"],
+        api_url=data["endpoints"]["rest"],
+    )
     res = auth_client.get_accounts()
     print(json.dumps(res, indent=4, sort_keys=True))
     res = auth_client.get_products()
     print(json.dumps(res, indent=4, sort_keys=True))
+    res = auth_client.get_product_ticker('BTC-USD')
+    print(json.dumps(res, indent=4, sort_keys=True))
+    # res = auth_client.get_product_trades('BTC-USD')
+    # print(json.dumps(res, indent=4, sort_keys=True))
