8e92bacef7d80603b5e0678f77fc9f007fbf8b21
chesswiz16 <3wood.tiger@gmail.com>
2018-01-18 02:48:50 -0500

    Yeah, bash isn't my strong suit

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-retry() {
-    local -r cmd="$@"
-    local -i attempt_num=1
-
-    until $cmd
-    do
-        echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
-        sleep $(( attempt_num ))
-    done
-}
-
-retry "python3 start_cost_basis.py config/prod.json ETH-USD"
-retry "python3 balance_logger.py config/prod.json"
+python3 start_cost_basis.py config/prod.json ETH-USD &
+python3 balance_logger.py config/prod.json &
