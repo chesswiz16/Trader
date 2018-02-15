1201f7172ffa4a366c3358fc4796873200208b23
chesswiz16 <3wood.tiger@gmail.com>
2018-02-14 23:25:49 -0500

    Check orders didn't work

--- trader/cost_basis.py
+++ trader/cost_basis.py
@@ -142,11 +142,7 @@ class CostBasisTrader(Trader):
             self.product_id, cost_basis, self.base_currency_bought, self.quote_currency_paid, self.current_order_depth))
 
     def check_orders(self, orders):
-        Trader.check_orders(self, orders)
-        if len(orders) < 2 and self.current_order_depth <= self.max_order_depth:
-            # We're missing an order, reset based on known cost basis
-            module_logger.warning('{}|Unexpected order state, recovering'.format(self.product_id))
-            self.place_bracket_orders()
+        pass
 
     def place_next_orders(self, settled_order):
         """Meat of the logic, place new orders as described in __init__.
